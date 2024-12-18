9dbe4e016f3e370fc072b861ebb75713f2961a36
kushal721 <bhattakushal1@gmail.com>
2024-12-18 22:48:41 +0545

    Header

--- ecommerce-frontend/src/Components/Header/index.js
+++ ecommerce-frontend/src/Components/Header/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import Logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
 import CountryDropdown from "../CountryDropdown";
+import { IoIosSearch } from "react-icons/io";
+import Button from "@mui/material/Button";
+import { FiUser } from "react-icons/fi";
+import { IoBagOutline } from "react-icons/io5";
+import SearchBox from "./SearchBox";
 
 const Header = () => {
   return (
@@ -16,7 +21,7 @@ const Header = () => {
           </div>
         </div>
 
-        <div className="header">
+        <header className="header">
           <div className="container">
             <div className="row">
               <div className="logoWrapper d-flex align-items-center col-sm-2">
@@ -26,10 +31,42 @@ const Header = () => {
               </div>
               <div className="col-sm-10 d-flex align-items-center part2">
                 <CountryDropdown />
+
+                <SearchBox />
+
+                <div className="part3 d-flex align-items-center ml-auto">
+                  <Button className="circle mr-3">
+                    <FiUser />
+                  </Button>
+                  <div className="ml-auto cartTab d-flex align-items-center">
+                    <span className="price">$3.29</span>
+                    <div className="position-relative ml-2">
+                      <Button className="circle ">
+                        <IoBagOutline />
+                      </Button>
+                      <span className="count d-flex align-items-center justify-content-center">
+                        1
+                      </span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </header>
+
+        <nav>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-3 navPart1">
+                <Button className="allCatTap">
+
+                </Button>
+              </div>
+              <div className="col-sm-9 navPart2"></div>
+            </div>
+          </div>
+        </nav>
       </div>
     </>
   );
