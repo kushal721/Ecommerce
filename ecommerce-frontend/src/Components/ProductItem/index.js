9bf8fea74458dbd24fe7462ead3358cb14ed261e
kushal721 <bhattakushal1@gmail.com>
2025-01-09 20:47:27 +0545

    Productmodal

--- ecommerce-frontend/src/Components/ProductItem/index.js
+++ ecommerce-frontend/src/Components/ProductItem/index.js
@@ -1,50 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import { SlSizeFullscreen } from "react-icons/sl";
 import Rating from "@mui/material/Rating";
 import { FaRegHeart } from "react-icons/fa";
 import ProductModal from "../ProductModal";
 const ProductItem = () => {
+  const [isOpenProductModal, setisOpenProductModal] = useState(false);
+  const viewProductDetails = (id) => {
+    setisOpenProductModal(true);
+  };
+
+  const closeProductModal = () => {
+    setisOpenProductModal(false);
+  };
   return (
     <>
-    <div className="product2">
-      <div className="item productItem">
-        <div className="imgWrapper">
-          <img
-            src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg"
-            alt="Product 1"
-            className="w-100 "
-          />
-          <span className="badge badge-primary">28%</span>
-          <div className="actions">
-            <Button>
-              <SlSizeFullscreen />
-            </Button>
-            <Button>
-              <FaRegHeart  style={{fontSize:'20px'}}/>
-            </Button>
+      <div className="product2">
+        <div className="item productItem">
+          <div className="imgWrapper">
+            <img
+              src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg"
+              alt="Product 1"
+              className="w-100 "
+            />
+            <span className="badge badge-primary">28%</span>
+            <div className="actions">
+              <Button onClick={() => viewProductDetails(1)}>
+                <SlSizeFullscreen />
+              </Button>
+              <Button>
+                <FaRegHeart style={{ fontSize: "20px" }} />
+              </Button>
+            </div>
           </div>
-        </div>
-        <div className="info">
-          <h4>Werther's Orginal Caramel Hard Candies</h4>
-          <span className="text-success d-block">In Stock</span>
-          <Rating
-            className="mt-2 mb-2"
-            name="read-only"
-            value={5}
-            readOnly
-            size="small"
-            precision={0.5}
-          />
-          <div className="d-flex">
-            <span className="oldPrice">$20.00</span>
-            <span className="netPrice text-danger ml-2">$20.00</span>
+          <div className="info">
+            <h4>Werther's Orginal Caramel Hard Candies</h4>
+            <span className="text-success d-block">In Stock</span>
+            <Rating
+              className="mt-2 mb-2"
+              name="read-only"
+              value={5}
+              readOnly
+              size="small"
+              precision={0.5}
+            />
+            <div className="d-flex">
+              <span className="oldPrice">$20.00</span>
+              <span className="netPrice text-danger ml-2">$20.00</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <ProductModal />
+      {isOpenProductModal === true && (
+        <ProductModal closeProductModal={closeProductModal} />
+      )}
+
+      {/* <ProductModal /> */}
     </>
   );
 };
--- ecommerce-frontend/src/Components/ProductModal/index.js
+++ ecommerce-frontend/src/Components/ProductModal/index.js
@@ -1,22 +1,98 @@
-import React from 'react'
+import React, { useRef } from "react";
+import Dialog from "@mui/material/Dialog";
+import { IoIosSearch } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+import Button from "@mui/material/Button";
+import Rating from "@mui/material/Rating";
+import Slider from "react-slick";
+import InnerImageZoom from "react-inner-image-zoom";
+import "react-inner-image-zoom/lib/InnerImageZoom/styles.css";
+const ProductModal = (props) => {
 
-const ProductModal = () => {
+  var settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: false,
+    arrows: true,
+  };
+
+  var settings2 = {
+    dots: false,
+    infinite: false,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: false,
+    arrows: false,
+  };
+
+  const zoomSliderBig = useRef();
+  const zoomSlider = useRef();
   return (
-<>
-<Dialog
+    <>
+      <Dialog
         open={true}
-        
-       
+        className="productModal"
+        onClose={() => props.closeProductModal()}
       >
-       
-        <Button className="close_" >
+        <Button className="close_" onClick={() => props.closeProductModal()}>
           <IoMdClose />
         </Button>
-        
-      
-      </Dialog>
-</>
-  )
-}
+        <h4 className="mb-1 font-weight-bold">
+          {" "}
+          All Natural Italian-Syle Chicken Meatballs
+        </h4>
+        <div className="d-flex align-items-center">
+          <div className="d-flex align-items-center mr-4">
+            <span>Brands:</span>
+            <span className="ml-2">
+              <b>Welch's</b>
+            </span>
+          </div>
 
-export default ProductModal
+          <Rating
+            name="read-only"
+            value={5}
+            size="small"
+            precision={0.5}
+            readOnly
+          />
+        </div>
+        <hr />
+        <div className="row mt-2 productDetailsModel">
+          <div className="col-md-5">
+            <div className="productZoom">
+              <Slider {...settings2} class="zoomSliderBig" ref={zoomSliderBig}>
+                <div className="item">
+                  <InnerImageZoom
+                    zoomType="hover"
+                    zoomScale={1}
+                    src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62.jpg"
+                  />
+                </div>
+              </Slider>
+            </div>
+
+            {/* <Slider {...settings2} class ='zoomSlider' ref={zoomSlider} > */}
+            <Slider {...settings2} class="zoomSlider" ref={zoomSlider}>
+              <div className="item">
+                <img
+                  src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62.jpg"
+                  alt=""
+                  className="w-100"
+                />
+              </div>
+            </Slider>
+          </div>
+
+          <div className="col-md-7"></div>
+        </div>
+      </Dialog>
+    </>
+  );
+};
+
+export default ProductModal;
